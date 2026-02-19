d93c0b02f2596ae6f2836c3e500f556c01e0763d
devhamaad <devhamaad@gmail.com>
2026-02-20 01:00:52 +0500

    Spring Boot project setup from Spring Initializr

---
+++ src/main/java/com/devhamaad/Personalized/Quote/Generator/API/with/SQ/PersonalizedQuoteGeneratorApiWithSqApplication.java
@@ -0,0 +1,13 @@
+package com.devhamaad.Personalized.Quote.Generator.API.with.SQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizedQuoteGeneratorApiWithSqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizedQuoteGeneratorApiWithSqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devhamaad/Personalized/Quote/Generator/API/with/SQ/PersonalizedQuoteGeneratorApiWithSqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhamaad.Personalized.Quote.Generator.API.with.SQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizedQuoteGeneratorApiWithSqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
